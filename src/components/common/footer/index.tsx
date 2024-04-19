27bbee2b89cd1d2e5602834d8bd7edd4454275ba
vvkkr1 <vivek.kumarr1107@gmail.com>
2024-04-19 19:52:00 +0530

    styled-components-added

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FooterWrapper, ContainerWrapper } from './styles';
+
+function Footer(){
+    return(
+        <FooterWrapper>
+            <ContainerWrapper>
+                <h3>Hello World!!!</h3>
+            </ContainerWrapper>
+        </FooterWrapper>
+    )
+}
+export default Footer
---
+++ src/components/common/footer/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+    background: gray;
+`; 
+
+export const ContainerWrapper = styled.div``;
